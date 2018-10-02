265548509930097d51a284fbd128d838da4773a6
Vijaya Kumar V <vijayvulchi@users.noreply.github.com>
2018-10-02 17:21:16 +0530

    functional part of billing system almost ready

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,21 @@
-function item(initialItem) {
+function item(initialItem, initialQuantity) {
     var self = this;
     self.item = ko.observable(initialItem);
-    console.log(self.item());
+    self.quantity = ko.observable(initialQuantity);
+	self.formattedPrice = ko.computed(function() {
+		var price = self.item().price * self.quantity().quantity;
+		return price ? "$" + price.toFixed(2) : "None";        
+	});
 }
 
 function viewModel() {
 	// Data
 	var self = this;
-	self.jsonData = ko.observableArray();
-	self.items = ko.observableArray()
 	self.appTitle = ko.observable();
 	self.appSubTitle = ko.observable();
+	self.jsonData = ko.observableArray();
+	self.items = ko.observableArray()
+	self.addNewItem = ko.observable();
 
 	// Load initial state from server
 	$.getJSON('../data/billing.json', function(data, textStatus, jqXHR) {
@@ -27,13 +32,20 @@ function viewModel() {
     function populateData () {
 	    self.appTitle(self.jsonData().appTitle);
 	    self.appSubTitle(self.jsonData().appSubTitle);
-
-	    // Editable data
-	    self.items([
-	        new item(self.jsonData().vegetables[0]),
-	        new item(self.jsonData().vegetables[1])
-	    ]);
+	    // Operations
+	    self.addNewItem = function() {
+	        self.items.push(new item(self.jsonData().vegetables[0], self.jsonData().quantity[0]));
+	    }
+	    self.removeItem = function(item) { self.items.remove(item) }
     }
+
+	self.totalAmount = ko.computed(function() {
+		var total = 0;
+		for (var i = 0; i < self.items().length; i++) {
+			total += self.items()[i].item().price;
+		}
+		return total;
+	});
 
 	self.fullAppName = ko.computed(function () {
 		return self.appTitle() + ' ' + self.appSubTitle();
